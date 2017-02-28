5d09052caeae50bfdd0329d0c007603217c8516e
bryan-brancotte <bryan.brancotte@gmail.com>
2017-02-28 15:41:44 +0100

    clean up

--- src/slipstream/api/api.py
+++ src/slipstream/api/api.py
@@ -389,12 +389,6 @@ class Api(object):
                 root.set('isBase', 'false')
                 root.find('cloudImageIdentifiers').clear()
 
-        if module_reference_uri is not None:
-            root.set('moduleReferenceUri', module_reference_uri)
-            if not keep_both_module_reference_uri_and_cloud_identifiers:
-                root.set('isBase', 'false')
-                root.find('cloudImageIdentifiers').clear()
-
         if cloud_identifiers is not None:
             cloud_image_identifiers = root.find('cloudImageIdentifiers')
             for cloud, identifier in cloud_identifiers.items():
@@ -443,7 +437,6 @@ class Api(object):
                               disk=node.get('disk'),
                               extra_disk_volatile=node.get('extraDiskVolatile'),
                               )
-
 
     def list_project_content(self, path=None, recurse=False):
         """
