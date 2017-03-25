2068cb0b3a06749796699510032730c34992f1c5
lionel <lionel@sixsq.com>
2017-03-25 19:01:32 +0100

    bugfix

--- src/slipstream/api/api.py
+++ src/slipstream/api/api.py
@@ -494,7 +494,7 @@ class Api(object):
                                     status=elem.get('status').lower(),
                                     started_at=elem.get('startTime'),
                                     last_state_change=elem.get('lastStateChangeTime'),
-                                    cloud=elem.get('cloudServiceNames'),
+                                    clouds=elem.get('cloudServiceNames','').split(','),
                                     username=elem.get('username'),
                                     abort=elem.get('abort'),
                                     service_url=elem.get('serviceUrl'),
