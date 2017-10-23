81af89900c72af7dbe58f895662fef5df42ba1d5
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-10-23 11:28:01 +0200

    WIP: bumped version, minor changes to logging.

--- api/src/slipstream/api/log.py
+++ api/src/slipstream/api/log.py
@@ -1,24 +1,22 @@
 import logging
 
-# Mapping between logging levels and CLI verbosity levels.
-# Default WARNING
-# -v INFO
-# -vv DEBUG
-# -vvv DEBUG + HTTP request and body
-
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.WARNING
+LOG_FILE = 'slipstream-api.log'
 
 FORMAT_FIELD_SEP = ' '
 FORMAT = '%(asctime)s{0}%(name)s{0}%(levelname)s{0}%(message)s'.format(
     FORMAT_FIELD_SEP)
 FORMAT_DATE = '%Y-%m-%dT%H:%M:%SZ'
 
-logging.basicConfig(format=FORMAT, datefmt=FORMAT_DATE, level=LOG_LEVEL)
 
-
-def get_logger(name=__name__, log_level=LOG_LEVEL):
+def get_logger(name=__name__, log_level=LOG_LEVEL, stream=None):
+    if stream:
+        logging.basicConfig(format=FORMAT, datefmt=FORMAT_DATE,
+                            level=log_level, stream=stream)
+    else:
+        logging.basicConfig(filename=LOG_FILE, format=FORMAT,
+                            datefmt=FORMAT_DATE, level=log_level)
     logger = logging.getLogger(name)
-    logger.setLevel(log_level)
     return logger
 
 
