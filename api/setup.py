926a4e049e06553cfe233f18540f713b283928b7
Khaled Basbous <khaled@sixsq.com>
2018-02-12 16:56:03 +0100

    rpm built and add release to pypi documentation (#64)
    * rpm builded and add release to pypi documentation
    
    * activate on /usr/local/bin/rpmbuild as well
    
    * tidy up the readme a bit
    
    * more tidying
    
    * add newline

--- api/setup.py
+++ api/setup.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
-import ast
-import re
 import sys
 
 from setuptools import find_packages, setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('src/slipstream/api/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-install_requires = [
-    'defusedxml',
-    'requests',
-    'six',
-]
+with open('requirements.txt') as f:
+    install_requires = []
+    for line in f.readlines():
+        if not line.startswith('mock'):
+            install_requires.append(line)
 
 if sys.version_info < (3, 2):
     install_requires.append('configparser')
+
+version = '${project.version}'
+packages = find_packages(where='src/')
 
 setup(
     name='slipstream-api',
@@ -36,18 +31,19 @@ setup(
     include_package_data=True,
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development'
     ],
 )
